7ae5acc9331275e54346d219d72c81c47be6525f
shanehoey <shanehoey@users.noreply.github.com>
2018-02-24 22:23:41 +1000

    Added backup script

---
+++ scripts/example-5-backup-mediant.ps1
@@ -0,0 +1,10 @@
+#requires -version 5.0
+#requires -module Mediant
+
+import-module mediant 
+
+$mediantcredential = Get-Credential -UserName "user" -Message "Mediant Credential"
+
+$mediantdevice = Get-MediantDevice -Mediant 172.30.69.146 -Credential $mediantcredential -Http http
+
+Get-MediantDeviceStatus -MediantDevice $mediantdevice 
